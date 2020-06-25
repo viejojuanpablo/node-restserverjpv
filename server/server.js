f2afe4f31d01c5c76ee7c6473673633ba7907d50
juan <viejojuanpablo@gmail.com>
2020-06-25 18:35:04 -0300

    first commit

--- server/server.js
+++ server/server.js
@@ -48,5 +48,5 @@ app.delete('/usuario', function(req, res) {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Escuchando en el puerto ${process.env.PORT}`);
+    console.log(`Escuchando en el spuerto ${process.env.PORT}`);
 });
